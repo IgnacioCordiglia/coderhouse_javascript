bf4723614967b65b9148819e0812e7b68cecbc31
Ignacio Cordiglia <ignaciocordiglia.contactos@gmail.com>
2022-07-23 16:50:49 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,54 @@
+let local= prompt("Ingrese nombre del equipo local");
+let visitante= prompt("Ingrese nombre del equipo visitante");
+let golesLocales = prompt("Ingrese los goles del equipo local: ");
+let golesVisitantes = prompt("Ingrese los goles del equipo visitante: ");
+//goles a favor del local
+let L_GF=0;
+//goles a favor del visitante
+let V_GF=0;
+//goles en contra del local
+let L_GC=0;
+//goles en contra del visitante
+let V_GC=0;
+//diferencia de gol del local
+let L_DG=0;
+//diferencia de gol del local
+let V_DG=0;
+//puntos del local
+let L_ptos=0;
+//puntos del visitante
+let V_ptos=0;
 
-var int = new Number(prompt("Ingrese un numero"))
-let mensaje="Los 10 numeros que le siguen a "+int+" son:"
-let lista=""
-
-for(var i=1; i<=10; i++) {
-    var suma=new Number(int+i)
-    if(i!=10)
-        lista=lista + suma + ", "
-        else {
-            lista=lista + suma
-        }
+if(golesLocales>golesVisitantes) {
+    L_ptos=3;
 }
+else if (golesVisitantes>golesLocales) {
+        V_ptos=3;
+     }
+     else if (golesLocales==golesVisitantes) {
+            L_ptos=1;
+            V_ptos=1;
+     }
 
-alert(mensaje + lista)
+L_GF+=golesLocales;
+L_GC+=golesVisitantes;
+V_GF+=golesVisitantes;
+V_GC+=golesLocales;
+L_DG=L_GF-L_GC;
+V_DG=V_GF-V_GC;
+
+//Estadisticas del local
+console.log("Estadisticas de "+local);
+console.log("Puntos: "+L_ptos);
+console.log("Goles a Favor: "+L_GF);
+console.log("Goles en contra: "+L_GC);
+console.log("Diferencia de Gol: "+L_DG);
+
+console.log(" ");
+
+//Estadisticas del visitante
+console.log("Estadisticas de "+visitante);
+console.log("Puntos: "+V_ptos);
+console.log("Goles a Favor: "+V_GF);
+console.log("Goles en contra: "+V_GC);
+console.log("Diferencia de Gol: "+V_DG);
