b313138574fa487a4cff49d00846b1575f775346
Ignacio Cordiglia <ignaciocordiglia.contactos@gmail.com>
2022-08-22 20:08:19 -0300

    metodos actualizar y borrarPartido optimizados

--- js/partido.js
+++ js/partido.js
@@ -27,26 +27,17 @@ class Partido {
     actualizar() {
         if(golesLocales!=null && golesVisitantes!=null) {
             //si gana el local, se le suma un partido ganado y un partido perdido al visitante
-            if(this.golesLocal>this.golesVisitante) {
-                this.visitante.PP++;
-                this.local.PG++;
-            }
+            this.golesLocal>this.golesVisitante ? (this.visitante.PP++, this.local.PG++) : null;
             //si gana el visitante, se le suma un partido ganado y un partido perdido al local
-            else if (this.golesVisitante>this.golesLocal) {
-                    this.visitante.PG++;
-                    this.local.PP++;
-                 }
-                 //si empatan, se le suma un partido empata a c/u
-                 else if (this.golesLocal==this.golesVisitante) {
-                            this.local.PE++;
-                            this.visitante.PE++;
-                      }
+            this.golesVisitante>this.golesLocal ? (this.visitante.PG++,this.local.PP++) : null;
+            //si empatan, se le suma un partido empata a c/u
+            this.golesLocal==this.golesVisitante ? (this.local.PE++, this.visitante.PE++) : null;
     
             //se le agregan los goles metidos y concedidos a los goles a favor y en contra de cada equipo
             this.local.GF=parseInt(this.local.GF) + parseInt(this.golesLocal);
-            this.visitante.GF=parseInt(this.visitante.GF) + parseInt(this.golesVisitante);
-            this.local.GC=parseInt(this.local.GC) + parseInt(this.golesVisitante);
-            this.visitante.GC=parseInt(this.visitante.GC) + parseInt(this.golesLocal)
+            this.visitante.GF= parseInt(this.visitante.GF) + parseInt(this.golesVisitante);
+            this.local.GC= parseInt(this.local.GC) + parseInt(this.golesVisitante);
+            this.visitante.GC= parseInt(this.visitante.GC) + parseInt(this.golesLocal)
             //se le suma un partido jugado mas a ambos
             this.local.PJ++;
             this.visitante.PJ++;
@@ -56,26 +47,19 @@ class Partido {
     borrarPartido() {
         if(this.golesLocal!=null && this.golesVisitante!=null) {
             //si ganó el local, se le resta el partido ganado y el partido perdido al visitante
-            if(this.golesLocal>this.golesVisitante) {
-                this.visitante.PP--;
-                this.local.PG--;
-            }
+            this.golesLocal>this.golesVisitante ? (this.visitante.PP--, this.local.PG--) : null;
+            
             //si ganó el vistante, se le resta el partido ganado y el partido perdido al local
-            else if (this.golesVisitante>this.golesLocal) {
-                    this.visitante.PG--;
-                    this.local.PP--;
-                 }
-                 //si empataron, se le resta el partido empatado a ambos
-                 else if (this.golesLocal==this.golesVisitante) {
-                            this.local.PE--;
-                            this.visitante.PE--;
-                      }
+            this.golesVisitante>this.golesLocal ? (this.visitante.PG--,this.local.PP--) : null;
+    
+            //si empataron, se le resta el partido empatado a ambos
+            this.golesVisitante==this.golesLocal ? (this.visitante.PE--, this.visitante.PE--): null;
     
             //se le quitan los goles metidos y concedidos a los goles a favor y en contra de cada equipo
-            this.local.GF=parseInt(this.local.GF) - parseInt(this.golesLocal);
-            this.visitante.GF=parseInt(this.visitante.GF) - parseInt(this.golesVisitante);
-            this.local.GC=parseInt(this.local.GC) - parseInt(this.golesVisitante);
-            this.visitante.GC=parseInt(this.visitante.GC) - parseInt(this.golesLocal)
+            this.local.GF-= parseInt(this.golesLocal);
+            this.visitante.GF-= parseInt(this.golesVisitante);
+            this.local.GC-= parseInt(this.golesVisitante);
+            this.visitante.GC-= parseInt(this.golesLocal);
             //se le resta un partido jugado mas a ambos
             this.local.PJ--;
             this.visitante.PJ--;
